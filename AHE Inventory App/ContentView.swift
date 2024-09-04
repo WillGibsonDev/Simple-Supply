6df0551920a23aed4f0f5428420e8c9af39445dc
wgibson002 <91209879+wgibson002@users.noreply.github.com>
2024-09-04 14:00:01 -0600

    Initial Commit

---
+++ AHE Inventory App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AHE Inventory App
+//
+//  Created by Will Gibson on 9/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
