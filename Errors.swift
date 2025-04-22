a34ecc3993b6b86fe4f668d44d8ff6a350d0d34c
WillGibsonDev <wgibsondev@gmail.com>
2025-04-22 17:52:04 -0600

    Test

--- Errors.swift
+++ Errors.swift
@@ -2,9 +2,10 @@
 //  Errors.swift
 //  AHE Simple Supply
 //
-//  Created by Will Gibson on 4/3/25.
+//  Created by Will Gibson on 4/22/25.
 //
 
 import Foundation
+
 
 //TODO: Implement Error types and improve Alerts on the UI.
